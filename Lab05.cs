9613a78e38a29b9ccc8749149d34d4d3900a5e73
Dimetil <70803321+DimetilTriptAmin@users.noreply.github.com>
2020-10-24 10:44:08 +0300

    Add files via upload

---
+++ Lab05.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Испытание, Тест, Экзамен, Выпускной экзамен, Вопрос;
+
+namespace OOP_Lab05
+{
+    interface IGrading
+    {
+       int GetMark();
+    }
+    abstract class Assay : IGrading
+    {
+        private int _questionAmount;
+        private Question[] QuestionArray;
+
+    }
+
+    class Test : Assay
+    {
+
+    }
+
+    class Exam : Assay
+    {
+
+    }
+
+    class Final_Exam : Exam
+    {
+
+    }
+
+    sealed class Question
+    {
+        private string _question;
+        private string _answer;
+    }
+}
